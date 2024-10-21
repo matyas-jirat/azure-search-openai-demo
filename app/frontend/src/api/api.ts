cbf787ec34b9774e146321b3fe4cb986bdbc4b3c
Matyas Jirat <matyas.jirat@bytegarden.com>
2024-10-21 21:39:31 +0200

    test for change of generation of url

--- app/frontend/src/api/api.ts
+++ app/frontend/src/api/api.ts
@@ -79,7 +79,7 @@ export async function getSpeechApi(text: string): Promise<string | null> {
 }
 
 export function getCitationFilePath(citation: string): string {
-    return `${BACKEND_URI}/content/${citation}`;
+    return `https://gen2sharepoint.blob.core.windows.net/gptkbcontainer/${citation}`;
 }
 
 export async function uploadFileApi(request: FormData, idToken: string): Promise<SimpleAPIResponse> {
